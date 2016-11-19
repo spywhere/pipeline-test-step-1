0b6b13c8711bb46e6a53afbf22fdb3603b075e4d
Sirisak Lueangsaksri <spywhere@me.com>
2016-11-19 14:43:25 +0700

    Specify working directory

--- script.js
+++ script.js
@@ -4,8 +4,15 @@ execSync("git config --global push.default simple");
 execSync("git config --global user.email \"noreply@travis-ci.org\"");
 execSync("git config --global user.name \"Travis Pipeline\"");
 execSync("git clone https://$GH_TOKEN@github.com/spywhere/pipeline-test-step-2a.git");
-execSync("cd pipeline-test-step-2a");
-execSync("touch $(date +%Y%m%d%H%M%S)");
-execSync("git add .");
-execSync("git commit -m \"add $(date +%Y%m%d%H%M%S) file\"");
-execSync("git push origin");
+execSync("touch $(date +%Y%m%d%H%M%S)", {
+    cwd: "./pipeline-test-step-2a"
+});
+execSync("git add .", {
+    cwd: "./pipeline-test-step-2a"
+});
+execSync("git commit -m \"add $(date +%Y%m%d%H%M%S) file\"", {
+    cwd: "./pipeline-test-step-2a"
+});
+execSync("git push origin", {
+    cwd: "./pipeline-test-step-2a"
+});
